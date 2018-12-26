c013e46ff80d1d40f3698c2a796273dab8416469
Ben Ipsen <email@benipsen.com>
2018-12-26 13:28:22 -0800

    initial index page rendered with next

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Head from 'next/head'
+const Index = () => (
+  <div>
+    <Head>
+      <title>The BIST Wedding - September 14th 2019</title>
+      <link rel="stylesheet" href="https://use.typekit.net/hfp3zvi.css"></link>
+    </Head>
+    <style global jsx>
+    {`
+      body {
+        font-family: urbane-rounded, sans-serif;
+        font-weight: '100';
+        font-style: normal;
+        text-align: center;
+        color: #cccccc;
+        margin: 72px;
+      }
+      h2 {
+        font-size: 25px;
+        font-weight: 100;
+        margin: 5px;
+      }
+    `}
+    </style>
+    <div>
+      <h2>Ben & Sarah's Wedding Website</h2> 
+      <p>...is coming really soon!</p>
+    </div>
+
+  </div>
+)
+
+export default Index
